cad36e5a87eaa2b872a2433a4a1a7461cf163cb3
Manibhadra-Singh <119622801+Manibhadrasingh@users.noreply.github.com>
2024-02-17 01:21:45 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [joke, setJoke] = useState({ setup: "Loading", punchline: "" });
+  const [loaded, setLoaded] = useState(false);
+  const hasMounted = useRef(false);
+
+  useEffect(() => {
+    if (!hasMounted.current) {
+      hasMounted.current = true;
+
+      axios
+        .get("https://official-joke-api.appspot.com/random_joke")
+        .then((response) => {
+          console.log(response);
+          setJoke({
+            setup: response.data.setup,
+            punchline: response.data.punchline,
+          });
+          setLoaded(true);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello Aliens, I am back</h2>
+      {loaded ? (
+        <>
+          <h4>{joke.setup}</h4>
+          <h4>{joke.punchline}</h4>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
